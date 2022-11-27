671444aa757a7f333b3b85484d86678827908811
Lau <laurentiucozma12@gmail.com>
2022-11-27 11:52:59 +0200

    hm

--- app/services/csv.php
+++ app/services/csv.php
@@ -1,16 +1,13 @@
 <?php
 function get_html($csv_file)
-{
-    $html = '<table>';
+{    
+    $data = [];
+    $html = '';
     $file = fopen($csv_file, 'r');
     
     // Skip the first line
     fgets($file); 
-    $html .= '<thead>';
-    $data = [];
 
-    $html .= '<thead>';
-    $html .= '<tbody>';
     while ($line = fgetcsv($file))
     {
         // Skip empty lines
@@ -29,22 +26,18 @@ function get_html($csv_file)
 
     // Delete the duplicates
     $data = array_unique($data, SORT_REGULAR);
-    // $data = array_filter($data, fn($value) => !is_null($value) && $value !== '');
 
     // Read the csv file
     for ($i = 0; $i < count($data); $i++)
     {       
         $html .= '<div>';
-        for ($j = 0; $j < 3; $j++)
-        {  
-            $html .= $data[$i][$j];
-        }
-        
+            for ($j = 0; $j < 3; $j++)
+            {  
+                $html .= $data[$i][$j];
+            }        
         $html .= '</div>';
     }
 
-    $html .= '</tbody>';
-    $html .= '</table>';
     return $html;
 }
 ?>
