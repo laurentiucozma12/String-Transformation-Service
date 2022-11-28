5176e2a51a21d1127a2fdb663f45a2b233d9c107
Lau <laurentiucozma12@gmail.com>
2022-11-28 13:41:24 +0200

    almost done

--- README.md
+++ README.md
@@ -63,7 +63,9 @@ http://projectName/app/pages/dashboard.php
     - [move_uploaded_file](https://www.php.net/manual/en/function.move-uploaded-file.php)
     - [usort()](https://www.php.net/manual/en/function.usort.php)
     - [array_unique](https://www.php.net/manual/en/function.array-unique.php)
+    - [array_push](https://www.php.net/manual/en/function.array-push.php)
     - [in_array()](https://www.php.net/manual/en/function.in-array.php)
+    - [is_string](https://www.php.net/manual/en/function.is-string.php)
 
 
 
--- app/services/csv.php
+++ app/services/csv.php
@@ -2,7 +2,7 @@
 function get_html($csv_file)
 {    
     $data = [];
-    $results = [];
+    $results = [];    
     $file = fopen($csv_file, 'r');
     
     // Skip the first line
@@ -14,28 +14,68 @@ function get_html($csv_file)
         $data[] = $line;
     }
 
-    // Sort alphabetically the data
+    // Sort data alphabetically
     usort($data, function($item1,$item2)
     {
         return $item1[0]<=>$item2[0];
-    });   
+    });  
 
-    // Read the data and print the results
-    $i = 0;
+    // Read the data
     foreach ($data as $info)
     {
-        if (!in_array($info, $results) && isset($info) && $info[0] != NULL)
+        if (!in_array($info, $results) && isset($info) && $info[0] !== NULL)
         {
             $results[] = $info;
-
-            for ($j = 0; $j < 3; $j++)
-            {
-                echo $results[$i][$j];             
-            }
-
-            echo '<br>';
-            $i++;
         }
+    }     
+
+    // Add the firtChar-lastChar chars in array
+    // Save in 2 variables the first and last word in the array (on the first column)
+    $firstWord = $results[0][0];
+    $lastWord = $results[count($results) - 1][0];
+
+    // Get the first char of the last and first words
+    $firstWordChar = $firstWord[0];
+    $lastWordChar = $lastWord[0];
+    foreach (range($firstWordChar, $lastWordChar) as $char)
+    {
+        $arrayChar = str_split($char);
+        array_push($results, $arrayChar); 
+    }
+
+    // Sort data alphabetically again 
+    usort($results, function($item1,$item2)
+    {
+        return $item1[0]<=>$item2[0];
+    });
+
+    // Print the data
+    $i = 0;
+    foreach ($results as $info)
+    {   
+        if (is_array($info) || is_object($info))
+        {        
+            foreach ($info as $word)
+            {     
+                echo $word;   
+            }
+            
+            echo '<br>';
+        }
+
+        $addBr = $info;
+        if (!is_array($addBr))
+        {        
+            str_split($addBr);
+        }
+
+        if (isset($addBr[$i][0]) && $addBr[$i][0] !== NULL 
+        && $addBr[$i][0] !== $addBr[$i + 1][0]) 
+        {
+            echo '<br>';
+        }        
+           
+        $i++;
     }
 }
 ?>
