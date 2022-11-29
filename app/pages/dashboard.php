e7d44d36e57bd4a5cea2c35e391900114c2a4a0f
Lau <laurentiucozma12@gmail.com>
2022-11-29 08:31:52 +0200

    frontend done

--- app/pages/dashboard.php
+++ app/pages/dashboard.php
@@ -5,33 +5,45 @@ include ROOT_PATH."/assets/html/head.php";
 include_once (ROOT_PATH."/app/services/csv.php");
 ?>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
-    <input type="file" name="csv" />
-    <input type="submit" name="upload" value="Upload CSV" />
-</form>
+<div class="container d-flex justify-content-center">
+    <div class="wrap p-2 col-12">
+        <div class="form-container p-2">
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data" class=" d-flex justify-content-between">
+                <label for="file-upload" class="custom-file-upload">
+                    Upload file
+                </label>
+                <input type="file" id="file-upload" name="csv" />
+                <label for="convert-file" class="custom-file-upload">
+                    Convert file
+                </label>
+                <input type="submit" id="convert-file" name="upload" value="Upload CSV" />
+            </form>
+        </div>
 
-<?php
-$display_table = '';
-if (isset($_POST['upload']) && $_POST['upload'] == 'Upload CSV')
-{
-    $upload_dir = ROOT_PATH."/assets/csv/uploads";
+        <div class="list-container d-flex justify-content-center m-4">
+            <?php
+            if (isset($_POST['upload']) && $_POST['upload'] == 'Upload CSV')
+            {
+                $upload_dir = ROOT_PATH."/assets/csv/uploads";
 
-    if ($_FILES['csv']['error'] == UPLOAD_ERR_OK)
-    {        
-        $tmp_name = $_FILES['csv']['tmp_name'];
+                if ($_FILES['csv']['error'] == UPLOAD_ERR_OK)
+                {        
+                    $tmp_name = $_FILES['csv']['tmp_name'];
 
-        // This is the chosen file
-        $name = basename($_FILES['csv']['name']);
+                    // This is the chosen file
+                    $name = basename($_FILES['csv']['name']);
 
-        // Moving csv file in "uploads" folder
-        $csvfile = $upload_dir.DIRECTORY_SEPARATOR.$name;
-        move_uploaded_file($tmp_name, $csvfile);
-        echo 'uploaded<br>';
-        
-        // Here comes the script which modifies the csv file
-        get_html($csvfile);
-    }
-}
-?>
+                    // Moving csv file in "uploads" folder
+                    $csvfile = $upload_dir.DIRECTORY_SEPARATOR.$name;
+                    move_uploaded_file($tmp_name, $csvfile);
+                    
+                    // Here comes the script which modifies the csv file
+                    get_html($csvfile);
+                }
+            }
+            ?>
+        </div>        
+    </div>
+</div>
 
 <?php include ROOT_PATH."/assets/html/footer.php" ?>
